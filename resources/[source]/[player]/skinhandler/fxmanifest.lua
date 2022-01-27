8c9db292b5f08b0ae88a509ccea6b3174a649432
Justus Braun <justusbraun03@gmail.com>
2022-01-27 14:53:15 +0100

    Added Skinhandler

---
+++ resources/[source]/[player]/skinhandler/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "skinhandler"
+description "Handles player skins"
+author "GFLP10#7754"
+version "1.0.0"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
